f46efff7ecd814a5c3dd60598bb6b9a187682468
jackie0922youhei <jackie0922youhei@gmail.com>
2021-02-05 19:22:27 +0900

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,17 +1,17 @@
 module SessionsHelper
-  
+
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
-  
+
   # ユーザーのセッションを永続的にする
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-  
+
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
@@ -24,25 +24,25 @@ module SessionsHelper
       end
     end
   end
-  
+
   # ユーザーがログインしていればtrue, その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
-  
+
   # 現在のユーザーをログアウトする
   def log_out
     session.delete(:user_id)
     @current_user = nil
   end
-  
+
   # 永続的セッションを破棄する
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
-  
+
   # 現在のユーザーをログアウトする
   def log_out
     forget(current_user)
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  
+
   def setup
     @user = users(:michael)
   end
-  
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -14,7 +14,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
-  
+
   test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email: @user.email, password: 'password' } }
@@ -34,5 +34,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user.id), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    # クッキーを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # クッキーを削除してログイン
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 end
