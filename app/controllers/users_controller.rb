80b71536eb5998579cea32a8dcfb83b2af53bb9a
jackie0922youhei <jackie0922youhei@gmail.com>
2021-02-06 19:25:15 +0900

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,14 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
-  
+
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def new
@@ -18,17 +19,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
       render 'new'
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
@@ -37,7 +38,7 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
@@ -49,9 +50,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
+
   # beforeアクション
-  
+
   # ログイン済みユーザーかどうか確認
   def logged_in_user
     unless logged_in?
@@ -60,13 +61,13 @@ class UsersController < ApplicationController
       redirect_to login_url
     end
   end
-  
+
   # 正しいユーザーかどうか確認
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
-  
+
   # 管理者かどうか確認
   def admin_user
     redirect_to(root_url) unless current_user.admin?
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,6 +5,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
     @other_user = users(:archer)
+    @non_activated_user = users(:non_activated)
   end
 
   test "should redirect index when not logged in" do
@@ -63,5 +64,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       delete user_path(@user)
     end
     assert_redirected_to root_url
+  end
+  
+  test "should not allow the not activated attribute" do
+    log_in_as (@non_activated_user)
+    assert_not @non_activated_user.activated?                                   # 有効化でないことを検証
+    get users_path
+    assert_select "a[href=?]", user_path(@non_activated_user), count: 0         # 非有効化ユーザーが表示されていないことを確認
+    get user_path(@non_activated_user)                                          # 非有効化ユーザーidのページを取得
+    assert_redirected_to root_url                                               # ルートurlにリダイレクトされればtrue
   end
 end
