c6337fb979b6ca754ed2c1622eb1a6ce4347b1d1
jackie0922youhei <jackie0922youhei@gmail.com>
2021-02-07 19:29:07 +0900

    access controll, creating micropost, original feed, delete micropost, test of them

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,6 +63,11 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+  
+  # 試作feedの定義
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   private
 
